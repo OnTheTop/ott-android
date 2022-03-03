a9ff256fb6b07abdd8d94d96736a571a1e89b711
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-03-04 01:34:08 +0900

    [Feat] SharedPrefernce 초기화

--- app/src/main/java/com/unithon/ott/App.kt
+++ app/src/main/java/com/unithon/ott/App.kt
@@ -1,7 +1,11 @@
 package com.unithon.ott
 
 import android.app.Application
+import com.unithon.ott.data.local.SharedPreference
 
 class App : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        SharedPreference.getInstance(this)
+    }
 }
