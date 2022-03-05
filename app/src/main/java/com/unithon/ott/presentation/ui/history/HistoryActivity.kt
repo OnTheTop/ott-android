25a84b630ff613634d83d329b629cdb43bddaff8
junieberry <junyoung44@g.skku.edu>
2022-03-05 18:16:57 +0900

    [Design] 홈 화면 기본 디자인

---
+++ app/src/main/java/com/unithon/ott/presentation/ui/history/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.unithon.ott.presentation.ui.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unithon.ott.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
